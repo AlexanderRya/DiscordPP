4c2f1c44effbaf2f090273468b4e2ac0cde73539
alex <alexandr.benbaccar@gmail.com>
2019-11-12 00:01:02 +0100

    ban command

--- main.cpp
+++ main.cpp
@@ -26,6 +26,12 @@ auto get_ws_url(const std::string& token) {
 
 static const std::string user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36";
 
+enum PERMS : std::size_t {
+	KICK_MEMBERS = 0x00000002,
+	BAN_MEMBERS = 0x00000004,
+	ADMIN = 0x00000008
+};
+
 class bot {
 	std::string name;
 	std::string token;
@@ -87,6 +93,21 @@ public:
 						nlohmann::json({}));
 	}
 
+	nlohmann::json get_guild_member_from_id(const snowflake guild_id, const snowflake member_id) {
+		auto r = cpr::Get(cpr::Url{ get_api_url()
+							  + "/guilds/" + std::to_string(guild_id)
+							  + "/members/" + std::to_string(member_id) },
+		                  cpr::Header{ { "User-Agent", user_agent } },
+		                  cpr::Header{ { "Authorization", "Bot " + token } });
+		return
+				r.status_code == 200
+				? nlohmann::json::parse(r.text)
+				: (static_cast<void>(
+						std::cout << "Error code: "
+						          << r.status_code << "\nWith body: " << r.text << "\n\n"),
+						nlohmann::json({}));
+	}
+
 	void ready_event_f(const nlohmann::json& j) {
 		session_id = j["d"]["session_id"].get<std::string>();
 		name = j["d"]["user"]["username"].get<std::string>();
@@ -101,21 +122,24 @@ public:
 				each["name"].get<std::string>()
 			});
 		}
-		gcv.emplace_back(std::pair<guild, std::vector<channel>>{ guild {
-			std::stoull(j["d"]["id"].get<std::string>()),
-			j["d"]["name"].get<std::string>()
-		}, std::vector<channel> {
-			std::move(temp)
-		} });
+		gcv.emplace_back(guild {
+				std::stoull(j["d"]["id"].get<std::string>()),
+				j["d"]["name"].get<std::string>()
+			}, std::vector<channel> {
+				std::move(temp)
+			});
 		std::cout << j["t"].get<std::string>()
 		        << "\nGUILD_NAME - GUILD_ID: "
 		        << gcv.back().first.name << " - " << gcv.back().first.id << "\n\n";
 	}
 	void message_create_f(const nlohmann::json& j) {
-		if (j["d"]["author"]["username"].get<std::string>() == "GayBot") return;
+		if (j["d"]["author"]["username"].get<std::string>() == name) return;
 		auto content = j["d"]["content"].get<std::string>();
 		if (content[0] == prefix) {
-			on_command(content, std::stoull(j["d"]["channel_id"].get<std::string>()));
+			on_command(std::stoull(j["d"]["author"]["id"].get<std::string>()),
+					content,
+					std::stoull(j["d"]["channel_id"].get<std::string>()),
+					std::stoull(j["d"]["guild_id"].get<std::string>()));
 			return;
 		}
 		auto guild_name = get_guild_from_id(
@@ -131,12 +155,7 @@ public:
 				content);
 	}
 	void message_update_f(const nlohmann::json& j) {
-		if (j["d"]["author"]["username"].get<std::string>() == name) return;
 		auto content = j["d"]["content"].get<std::string>();
-		if (content[0] == prefix) {
-			on_command(content, std::stoull(j["d"]["channel_id"].get<std::string>()));
-			return;
-		}
 		auto guild_name = get_guild_from_id(
 				std::stoull(j["d"]["guild_id"].get<std::string>()))["name"].get<std::string>();
 		auto channel_name = get_channel_from_id(
@@ -150,7 +169,7 @@ public:
 		        content);
 	}
 	void message_delete_f(const nlohmann::json&) {
-		std::cout << "DELETE_MESSAGE callback\n";
+		std::cout << "DELETE_MESSAGE callback\n\n";
 	}
 
 	void presence_update_f(const nlohmann::json& j) {
@@ -174,15 +193,59 @@ public:
 		std::cout << "PRESENCE_UPDATE callback\n\n";
 	}
 
-	void on_command(const std::string& command, const snowflake channel_id) {
+	void on_command(const snowflake sender, const std::string& command, const snowflake channel_id, const snowflake guild_id) {
 		auto body =
 				nlohmann::json {
 					{ "tts", false },
 				};
-		if (command == ">test") {
+		if (command == (prefix + "test"s)) {
 			body["content"] = "test";
-		} else if (command == ">mammt") {
+		} else if (command == (prefix + "mammt"s)) {
 			body["content"] = "no u";
+		} else if (command.find(prefix + "ban"s) != std::string::npos) {
+			auto cidx = command.find_first_of('>', 1);
+			if (cidx == std::string::npos) {
+				send_message("Error, invalid syntax, usage: "s + prefix + "ban `@user`", channel_id);
+				return;
+			}
+			size_t start_idx = cidx - 18;
+			size_t end_idx = cidx;
+			auto buser = get_guild_member_from_id(guild_id, std::stoull(std::string{ command.begin() + start_idx, command.begin() + end_idx }));
+			auto user = get_guild_member_from_id(guild_id, sender);
+			std::vector<std::pair<snowflake, u32>> groles;
+			auto temp = get_guild_from_id(guild_id)["roles"];
+			for (const auto& role : temp) {
+				groles.emplace_back(
+					std::stoull(role["id"].get<std::string>()),
+					(role["permissions"].get<u32>()));
+			}
+			bool b = false;
+			if (user["roles"].empty()) {
+				if (((groles[0].second & PERMS::ADMIN) == PERMS::ADMIN)
+				    || ((groles[0].second & PERMS::BAN_MEMBERS) == PERMS::BAN_MEMBERS)) {
+					//send_message("Can ban.", channel_id);
+					return;
+				} else {
+					send_message("Error, you don't have enough permissions to use the \">ban\" command.", channel_id);
+					return;
+				}
+			}
+			for (auto i = 0ul; i < user["roles"].size(); ++i) {
+				auto role_id = std::stoull(user["roles"][i].get<std::string>());
+				auto ridx = std::distance(groles.begin(), std::find_if(groles.begin(), groles.end(), [&](const auto& x) {
+					return x.first == role_id;
+				}));
+				if (((groles[ridx].second & PERMS::ADMIN) == PERMS::ADMIN)
+						|| ((groles[ridx].second & PERMS::BAN_MEMBERS) == PERMS::BAN_MEMBERS)) {
+						send_message("Can ban.", channel_id);
+						b = true;
+						break;
+				}
+			}
+			if (!b) {
+				send_message("Error, you don't have enough permissions to use the \">ban\" command.", channel_id);
+			}
+			return;
 		}
 		if (!body.contains("content")) {
 			body["content"] = "Unknown command.";
@@ -211,7 +274,10 @@ public:
 		                   cpr::Header{ { "Authorization", "Bot " + token } },
 		                   cpr::Body{ body });
 		if (r.status_code == 200) {
-			std::cout << "Send message callback: successfully sent message\n";
+			std::cout << "Send message callback: successfully sent message\n\n";
+		} else if (r.status_code == 429) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(nlohmann::json::parse(r.text)["retry_after"].get<int>()));
+			send_message(msg, channel_id);
 		} else {
 			std::cout << "Error: " << r.status_code << "\n" << "With body: " << r.text << "\n";
 		}
@@ -222,7 +288,7 @@ int main() {
 	int seq_number = 0;
 	websocket_client c;
 	std::string token;
-	std::ifstream f("token.txt");
+	std::ifstream f("../token.txt");
 	if (!f.is_open()) {
 		std::cout << "ERROR, couldn't open file.\n\n";
 		return -1;
